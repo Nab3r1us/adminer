2df3b7c9a031903de00ab028ffc52e2bc0b0c119
Nab3r1us <oleg.kostromin2011@gmail.com>
2023-12-01 15:26:56 +0300

    ui: login form (statusbar don't work)

---
+++ login.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace adminer
+{
+    partial class login
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.engine_comboBox = new System.Windows.Forms.ComboBox();
+            this.server_text = new System.Windows.Forms.TextBox();
+            this.login_text = new System.Windows.Forms.TextBox();
+            this.dbname_text = new System.Windows.Forms.TextBox();
+            this.password_text = new System.Windows.Forms.MaskedTextBox();
+            this.loggin_button = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 164);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(519, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.engine_comboBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.server_text, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.login_text, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.dbname_text, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.password_text, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.loggin_button, 1, 5);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(519, 163);
+            this.tableLayoutPanel1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Движок";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Сервер";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Имя пользователя";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(3, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Пароль";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "База данных";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // engine_comboBox
+            // 
+            this.engine_comboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.engine_comboBox.FormattingEnabled = true;
+            this.engine_comboBox.Items.AddRange(new object[] {
+            "MySQL",
+            "PostgreSQL",
+            "SQL Server"});
+            this.engine_comboBox.Location = new System.Drawing.Point(123, 3);
+            this.engine_comboBox.Name = "engine_comboBox";
+            this.engine_comboBox.Size = new System.Drawing.Size(393, 21);
+            this.engine_comboBox.TabIndex = 5;
+            // 
+            // server_text
+            // 
+            this.server_text.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.server_text.Location = new System.Drawing.Point(123, 28);
+            this.server_text.Name = "server_text";
+            this.server_text.Size = new System.Drawing.Size(393, 20);
+            this.server_text.TabIndex = 6;
+            // 
+            // login_text
+            // 
+            this.login_text.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.login_text.Location = new System.Drawing.Point(123, 53);
+            this.login_text.Name = "login_text";
+            this.login_text.Size = new System.Drawing.Size(393, 20);
+            this.login_text.TabIndex = 7;
+            // 
+            // dbname_text
+            // 
+            this.dbname_text.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dbname_text.Location = new System.Drawing.Point(123, 103);
+            this.dbname_text.Name = "dbname_text";
+            this.dbname_text.Size = new System.Drawing.Size(393, 20);
+            this.dbname_text.TabIndex = 8;
+            // 
+            // password_text
+            // 
+            this.password_text.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.password_text.Location = new System.Drawing.Point(123, 78);
+            this.password_text.Name = "password_text";
+            this.password_text.PasswordChar = '*';
+            this.password_text.Size = new System.Drawing.Size(393, 20);
+            this.password_text.TabIndex = 9;
+            // 
+            // loggin_button
+            // 
+            this.loggin_button.Location = new System.Drawing.Point(123, 128);
+            this.loggin_button.Name = "loggin_button";
+            this.loggin_button.Size = new System.Drawing.Size(175, 30);
+            this.loggin_button.TabIndex = 10;
+            this.loggin_button.Text = "Войти";
+            this.loggin_button.UseVisualStyleBackColor = true;
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 186);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(535, 225);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(535, 225);
+            this.Name = "login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Adminer : Вход";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox engine_comboBox;
+        private System.Windows.Forms.TextBox server_text;
+        private System.Windows.Forms.TextBox login_text;
+        private System.Windows.Forms.TextBox dbname_text;
+        private System.Windows.Forms.MaskedTextBox password_text;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button loggin_button;
+    }
+}
+
